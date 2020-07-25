88cb2d2e8fa27cd016a289066dd554dcb622ec35
Mario Rugeles <mrugeles@gmail.com>
2020-07-25 15:18:10 -0500

    update script

--- update_dataset.sh
+++ update_dataset.sh
@@ -6,5 +6,5 @@ cd ../etl
 python build_reports.py
 cd datasets/countries
 aws s3 cp . s3://co.data.covid19-us-east-2/countries/ --recursive
-
-
+cd ../../
+aws s3 cp processed_countries.csv s3://co.data.covid19-us-east-2
